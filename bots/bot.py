6e216e329439a40a2fdc59ca28ce5020763c1402
Sean <seanfinnessy@icloud.com>
2019-12-19 15:57:26 -0500

    Added face detection!

--- bots/bot.py
+++ bots/bot.py
@@ -16,36 +16,49 @@ def check_mentions(api, since_id):
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id, tweet_mode="extended").items():
         # set new since_id to the highest tweet id (most recent).
         new_since_id = max(tweet.id, new_since_id)
-        # checks if the the tweet is a reply (a mention), if it is, continue
+
+        '''
+        Im still trying to figure out what the below code does exactly...
+        
         if tweet.in_reply_to_status_id is not None:
+            logger.info("in_reply_to_status_id is not none... restart loop")
+            logger.info(tweet.in_reply_to_status_id)
             continue
+        logger.info("Detected mention...")
+        '''
 
         # if not following the person who mentioned you, follow them
         if not tweet.user.following:
             tweet.user.follow()
 
         # download media
-        media_ids = download_media(api, tweet)
+        if 'media' in api.get_status(tweet.in_reply_to_status_id).entities:
+            logger.info("I found media!")
+            media_ids = download_media(api, api.get_status(tweet.in_reply_to_status_id))
 
-        # tweets the status argument as a reply, to the specified tweet id
-        logger.info(f"Answering to {tweet.user.name} and posting profile pic...")
-        api.update_status(status=f"Hey {tweet.user.name}."
-                                 f"You have {tweet.user.followers_count} followers."
-                                 f"Here's the media you attached.",
-                          in_reply_to_status_id=tweet.id,
-                          auto_populate_reply_metadata=True,
-                          media_ids=media_ids
-                          )
+            # tweets the status argument as a reply, to the specified tweet id
+            logger.info(f"Answering to {tweet.user.name} and posting pic...")
+            api.update_status(status=f"Hey {tweet.user.name}. "
+                                     f"I tried to find some faces in the tweet you showed me.",
+                              in_reply_to_status_id=tweet.id,
+                              auto_populate_reply_metadata=True,
+                              media_ids=media_ids
+                              )
 
-        # delete downloaded media
-        if os.path.exists('C:\\PythonLearning\\TwitterBot\\media'):
-            files = glob.glob('.\\media\\*.jpg')
-            for f in files:
-                os.remove(f)
-            logger.info("Deleted downloaded photos...")
+            # delete downloaded media
+            if os.path.exists('C:\\PythonLearning\\TwitterBot\\media'):
+                files = glob.glob('.\\media\\*.jpg')
+                for f in files:
+                    os.remove(f)
+                logger.info("Deleted downloaded photos...")
+            else:
+                logger.info("Could not delete downloaded photo...")
         else:
-            logger.info("Could not delete downloaded photo...")
-
+            logger.info("Sorry could not find media..")
+            api.update_status(status=f"Hey {tweet.user.name}, sorry I could not find a photo in the tweet you tagged"
+                                     f" me in...",
+                              in_reply_to_status_id=tweet.id,
+                              auto_populate_reply_metadata=True)
     return new_since_id
 
 
--- bots/config.py
+++ bots/config.py
@@ -3,6 +3,7 @@ import logging
 import os
 import wget
 from PIL import Image, ImageOps, ImageEnhance
+import cv2
 
 logger = logging.getLogger()
 
@@ -29,36 +30,38 @@ def create_api():
 
 def download_media(api, tweet):
     media_files = set()
-    if 'media' in tweet.entities:
-        for media in tweet.extended_entities['media']:
-            print(media['media_url'])
-            media_files.add(media['media_url'])
 
-        logger.info("Beginning file download..")
-        media_ids = []
-        i = 0
-        for media_url in media_files:
-            wget.download(media_url, f'.\\media\\picture{i}.jpg')
-            image_manip(f'.\\media\\picture{i}.jpg')
-            res = api.media_upload(f'.\\media\\picture{i}.jpg')
-            media_ids.append(res.media_id)
-            i += 1
+    for media in tweet.extended_entities['media']:
+        print(media['media_url'])
+        media_files.add(media['media_url'])
 
-        return media_ids
-            
-    else:
-        print("Sorry, I could not find any attached media! Please DM me if I am wrong. Thanks!")
+    logger.info("Beginning file download..")
+    media_ids = []
+    i = 0
+    for media_url in media_files:
+        wget.download(media_url, f'.\\media\\picture{i}.jpg')
+        image_manip(f'.\\media\\picture{i}.jpg')
+        res = api.media_upload(f'.\\media\\picture{i}.jpg')
+        media_ids.append(res.media_id)
+        i += 1
+
+    return media_ids
 
 
 def image_manip(image):
+    face_detection(image)
+    '''
+    
+    Turned off while I test face detection
+    
     img = Image.open(image)
 
     # crush image
     img = img.convert('RGB')
     width, height = img.width, img.height
-    img = img.resize((int(width ** .75), int(height ** .75)), resample=Image.LANCZOS)
-    img = img.resize((int(width ** .88), int(height ** .88)), resample=Image.BILINEAR)
-    img = img.resize((int(width ** .9), int(height ** .9)), resample=Image.BICUBIC)
+    img = img.resize((int(width ** .93), int(height ** .93)), resample=Image.LANCZOS)
+    img = img.resize((int(width ** .95), int(height ** .95)), resample=Image.BILINEAR)
+    img = img.resize((int(width ** .97), int(height ** .97)), resample=Image.BICUBIC)
     img = img.resize((width, height), resample=Image.BICUBIC)
     img = ImageOps.posterize(img, 4)
 
@@ -67,11 +70,32 @@ def image_manip(image):
     logger.info(img.split()[0])
     r = ImageEnhance.Contrast(r).enhance(2.0)
     r = ImageEnhance.Brightness(r).enhance(1.5)
-    r = ImageOps.colorize(r, [194, 0, 0], [194, 0, 221])
+    r = ImageOps.colorize(r, [254, 0, 2], [255, 255, 15])
 
     # overlay red and yellow onto main image and sharpen
-    img = Image.blend(img, r, 0.75)
-    img = ImageEnhance.Sharpness(img).enhance(100.0)
+    # img = Image.blend(img, r, 0.75)
 
+    img = ImageEnhance.Sharpness(img).enhance(200.0)
+    img = ImageEnhance.Contrast(img).enhance(100.0)
+    img = ImageEnhance.Brightness(img).enhance(50.0)
+    img = ImageEnhance.Color(img).enhance(50.0)
     img.save(image, 'JPEG')
+    '''
+
+
+def face_detection(image):
+    original_image = cv2.imread(image)
+    grayscale_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier('C:\\PythonLearning\\TwitterBot\\bots\\haarcascade_frontalface_alt.xml')
+    detected_faces = face_cascade.detectMultiScale(grayscale_image)
+
+    for (column, row, width, height) in detected_faces:
+        cv2.rectangle(
+            original_image,
+            (column, row),
+            (column + width, row + height),
+            (0, 255, 0),
+            2
+        )
+    cv2.imwrite(image, original_image)
 
